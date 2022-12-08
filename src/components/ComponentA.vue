ef4aff802b90b00f3838fbd04d5d28b40b876e02
Samuel Rizal Harahap <samuel.rizal@bukalapak.com>
2022-12-08 21:40:42 +0700

    Init

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>Hello {{ msg }}! <span class="icon-heart" style="color: tomato"></span></div>
+</template>
